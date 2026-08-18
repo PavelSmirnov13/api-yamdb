bcf1baa7cd82af14f8d4f89b25f1313bd46ff3f0
Alexander Zhuravlev <zuravlevaleksandr675@gmail.com>
2026-08-18 05:58:13 +0500

    Добавлен пермишен IsAuthorOrModeratorOrAdminOrReadOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,3 +7,23 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user.is_authenticated and request.user.role == 'admin'
+
+
+class IsAuthorOrModeratorOrAdminOrReadOnly(permissions.BasePermission):
+    """Разрешает редактирование автору, модератору или админу."""
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (
+            obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
+            or request.user.is_superuser
+        )
