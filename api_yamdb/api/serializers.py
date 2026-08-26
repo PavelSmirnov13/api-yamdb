fe5aafb60f4116e6134d586606cdbcdd4ea04283
Pavel Smirnov <strong1389@gmail.com>
2026-08-26 16:25:43 +0700

    Fix conflict markers in all files

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from rest_framework import serializers
 from reviews.models import (
-<<<<<<< HEAD
-    Category, Genre, Title, Review, Comment, User, validate_username
-=======
     Category,
     Genre,
     Title,
@@ -11,7 +8,6 @@ from reviews.models import (
     Comment,
     User,
     validate_username,
->>>>>>> develop
 )
 
 
@@ -31,14 +27,6 @@ class GenreSerializer(serializers.ModelSerializer):
         fields = ('name', 'slug')
 
 
-<<<<<<< HEAD
-class TitleWriteSerializer(serializers.ModelSerializer):
-    """Сериализатор для создания и обновления Title."""
-
-    category = serializers.SlugRelatedField(
-        slug_field='slug',
-        queryset=Category.objects.all()
-=======
 class BaseTitleSerializer(serializers.ModelSerializer):
     """Базовый сериализатор для Title."""
 
@@ -57,80 +45,33 @@ class TitleWriteSerializer(BaseTitleSerializer):
 
     category = serializers.SlugRelatedField(
         slug_field='slug', queryset=Category.objects.all()
->>>>>>> develop
     )
     genre = serializers.SlugRelatedField(
         slug_field='slug',
         many=True,
-<<<<<<< HEAD
-        queryset=Genre.objects.all()
-    )
-    rating = serializers.IntegerField(read_only=True, default=None)
-
-    class Meta:
-        model = Title
-        fields = (
-            'id', 'name', 'year', 'rating',
-            'description', 'category', 'genre'
-        )
-
-
-class TitleReadSerializer(serializers.ModelSerializer):
-=======
         queryset=Genre.objects.all(),
         allow_empty=False,
     )
 
 
 class TitleReadSerializer(BaseTitleSerializer):
->>>>>>> develop
     """Сериализатор для чтения Title."""
 
     category = CategorySerializer()
     genre = GenreSerializer(many=True)
-<<<<<<< HEAD
-    rating = serializers.IntegerField(read_only=True, default=None)
-
-    class Meta:
-        model = Title
-        fields = (
-            'id', 'name', 'year', 'rating',
-            'description', 'category', 'genre'
-        )
-=======
->>>>>>> develop
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Review."""
 
     author = serializers.SlugRelatedField(
-<<<<<<< HEAD
-        slug_field='username',
-        read_only=True
-=======
         slug_field='username', read_only=True
->>>>>>> develop
     )
 
     class Meta:
         model = Review
         fields = ('id', 'text', 'author', 'score', 'pub_date')
 
-<<<<<<< HEAD
-    def validate(self, data):
-        request = self.context.get('request')
-        if request.method != 'POST':
-            return data
-        title_id = self.context['view'].kwargs.get('title_id')
-        if Review.objects.filter(
-            author=request.user, title_id=title_id
-        ).exists():
-            raise serializers.ValidationError(
-                'Вы уже оставили отзыв на это произведение.'
-            )
-        return data
-=======
     def validate(self, attrs):
         request = self.context.get('request')
         if request.method != 'POST':
@@ -141,19 +82,13 @@ class ReviewSerializer(serializers.ModelSerializer):
                 'Вы уже оставили отзыв на это произведение.'
             )
         return attrs
->>>>>>> develop
 
 
 class CommentSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Comment."""
 
     author = serializers.SlugRelatedField(
-<<<<<<< HEAD
-        slug_field='username',
-        read_only=True
-=======
         slug_field='username', read_only=True
->>>>>>> develop
     )
 
     class Meta:
@@ -166,11 +101,7 @@ class SignUpSerializer(serializers.Serializer):
 
     username = serializers.CharField(
         max_length=150,
-<<<<<<< HEAD
-        validators=[UnicodeUsernameValidator(), validate_username]
-=======
         validators=[UnicodeUsernameValidator(), validate_username],
->>>>>>> develop
     )
     email = serializers.EmailField(max_length=254)
 
@@ -188,16 +119,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-<<<<<<< HEAD
-            'username', 'email', 'first_name', 'last_name', 'bio', 'role'
-=======
             'username',
             'email',
             'first_name',
             'last_name',
             'bio',
             'role',
->>>>>>> develop
         )
 
 
--- api_yamdb/reviews/apps.py
+++ api_yamdb/reviews/apps.py
@@ -4,7 +4,4 @@ from django.apps import AppConfig
 class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
-<<<<<<< HEAD
-=======
     verbose_name = 'Отзывы'
->>>>>>> develop
